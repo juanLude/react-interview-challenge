1c79647b67cbc74355b875ed473ac86467acb6ff
Juan Ludevid <jmludevid@hotmail.com>
2024-01-05 15:45:58 +1100

    finish challenge

--- src/Component2.tsx
+++ src/Component2.tsx
@@ -2,17 +2,42 @@ import { useState } from "react";
 import "./Component2.css";
 export default function Component2() {
   const [cells, setCells] = useState(["a", "b", "c"]);
-
+  function changeCellContent(newCellValue: string, indexToUpdate: number) {
+    setCells((prevCells) =>
+      prevCells.map((cell, idx) =>
+        idx === indexToUpdate ? newCellValue : cell
+      )
+    );
+  }
+  function handlePlusClick(index: number) {
+    setCells((prevCells) => [
+      ...prevCells.slice(0, index + 1),
+      "_",
+      ...prevCells.slice(index + 1),
+    ]);
+  }
+  const joinedCells = cells.join("");
   return (
     <>
       <div>Component2</div>
       <main>
         {cells.map((cell, index) => (
           <div key={index} className="cell">
-            {cell}
+            <input
+              onChange={(e) => changeCellContent(e.currentTarget.value, index)}
+              value={cell}
+              type="text"
+            ></input>
+            {index < cells.length - 1 && (
+              <span
+                className="plus"
+                onClick={() => handlePlusClick(index)}
+              ></span>
+            )}
           </div>
         ))}
       </main>
+      <div style={{ margin: "10px" }}>{joinedCells}</div>
     </>
   );
 }
